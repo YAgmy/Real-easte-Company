2d5eb18d61037e313e65f5ca69daa5d29a9cf345
YAgmy <yasseragmy43@gmail.com>
2022-04-19 22:11:34 +0200

    solve responsive issue

--- js/main.js
+++ js/main.js
@@ -20,3 +20,19 @@ $('.services .servic').each( function () {
 });
     $(".services .servic").height(maxHeight);
 
+// img gallary
+        let nuofthum = $('.thumbnails').children().length,
+        margin = '0.5',
+        totalMargin = (nuofthum - 1) * margin,
+        thumbnailsWidth = (100 - totalMargin ) / nuofthum
+
+    $('.thumbnails img').css({
+        width: thumbnailsWidth + '%',
+        marginRight: totalMargin = 'px'
+    })
+
+$('.thumbnails img').on('click', function () {
+    $(this).addClass('act').siblings().removeClass('act');
+
+    $(".main-img img").attr('src', $(this).attr('src'));
+});
